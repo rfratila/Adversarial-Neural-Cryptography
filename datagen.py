2f512e6eaf20bb6760a92b8b874034fcd93a1358
Robert Fratila <fratilarobert@hotmail.com>
2017-04-05 11:19:24 -0400

    Message and Key generation completed

---
+++ datagen.py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+
+
+def get_plain_text(N=16,to_generate=256):
+    '''
+    Generates a plain text message in binary values
+    N: How many bits the message is
+    to_generate: How many messages to generate
+    '''
+    block_text = []
+    for i in range(0,to_generate):
+        text = []
+        for j in range(0,N):
+            text += [-1] if random.randint(0,1) == 0 else [1]
+        block_text += [text]
+    return np.array(block_text)
+
+def get_key(N=16):
+    '''
+    Generates a key value for Bob and Alice to use
+    N: How many bits the key is
+    '''
+    key = []
+    for j in range(0,N):
+        key += [-1] if random.randint(0,1) == 0 else [1]
+    return np.array([key])
+
+if __name__ == "__main__":
+    pass
