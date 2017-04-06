484909a652f3481ea0a3c3c376f94f005a7b7805
Robert Fratila <fratilarobert@hotmail.com>
2017-04-06 13:41:00 -0400

    Fixed Eve's network

--- neural_encryption.py
+++ neural_encryption.py
@@ -21,7 +21,7 @@ class Model:
 
     def create_network(self):
 
-        
+        self.strides = [1,2,1,1] if self.AB else [1,1,1,1]
         #Create a 2N x 2N deep dense layer for Alice/Bob and N x 2N if Eve 
         self.dense_depth = self.input_length-1 if self.AB else (2*self.input_length)-1
 
@@ -38,28 +38,30 @@ class Model:
         #Four conv layers for transforming the ciphertext
         self.conv1 = tf.layers.conv1d(inputs=self.dense_layers,
                                     filters=2, 
-                                    strides=[1],
+                                    strides=self.strides[0],
                                     kernel_size = [4], 
                                     padding='same',
                                     activation=tf.sigmoid,
                                     trainable = self.trainable)
+
         self.conv2 = tf.layers.conv1d(inputs=self.conv1,
                                     filters=4, 
-                                    strides=[2],
+                                    strides=self.strides[1],
                                     kernel_size = [2], 
                                     padding='same',
                                     activation=tf.sigmoid,
                                     trainable = self.trainable)
+
         self.conv3 = tf.layers.conv1d(inputs=self.conv2,
                                     filters=4, 
-                                    strides=[1],
+                                    strides=self.strides[2],
                                     kernel_size = [1], 
                                     padding='same',
                                     activation=tf.sigmoid,
                                     trainable = self.trainable)
         self.conv4 = tf.layers.conv1d(inputs=self.conv3,
                                     filters=1, 
-                                    strides=[1],
+                                    strides=self.strides[3],
                                     kernel_size = [1], 
                                     padding='same',
                                     activation=tf.tanh,
@@ -78,17 +80,25 @@ def main():
 
     #Inputs are [batch_size, image_width, image_height, channels]
     train_X = np.expand_dims(m_with_key,axis = 2) #prep for passing into network of size (batch,num_bits,1)
+    y = np.expand_dims(messages,axis = 2)
+
+    orig = tf.placeholder(dtype = tf.float32, shape=(None,num_bits,1))
 
     print ('Creating Alice net...')
     alice_net = Model(bit_count=num_bits,AB = True,training=True)
     print ('Creating Bob net...')
     bob_net = Model(bit_count=num_bits,AB = True,training=True)
     print ('Creating Eve net...')
-    e_net = Model(bit_count=num_bits,AB = False,training=True)
+    eve_net = Model(bit_count=num_bits,AB = False,training=True)
+
+    loss = tf.reduce_mean(tf.abs(tf.subtract(orig,alice_net.network)))
+    train_step = tf.train.AdamOptimizer(learning_rate=0.0008).minimize(loss)
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
+        import pudb; pu.db
         a = sess.run(alice_net.network, feed_dict={alice_net.input_layer: train_X})
+        sess.run(train_step, feed_dict={alice_net.input_layer: train_X,orig: y})
         print (train_X.shape)
         print (a.shape)
         print (train_X[0])
