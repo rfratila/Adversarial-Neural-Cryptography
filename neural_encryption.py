96a5f08f889b4d5704cb07dcb4dc5a04529aa88e
augustelalande <auguste.lalande@gmail.com>
2017-04-06 20:29:00 -0400

    add variable scoping to stop alice from training

--- neural_encryption.py
+++ neural_encryption.py
@@ -33,73 +33,80 @@ class Model:
         self.network = self.create_network()
 
     def create_network(self):
+        with tf.variable_scope(self.name):
 
-        self.strides = [1, 2, 1, 1] if self.AB else [1, 1, 1, 1]
-        # Create a 2N x 2N deep dense layer for Alice/Bob and N x 2N if Eve
-        self.dense_depth = self.input_length - 1 if self.AB else (2*self.input_length) - 1
+            self.strides = [1, 2, 1, 1] if self.AB else [1, 1, 1, 1]
+            # Create a 2N x 2N deep dense layer for Alice/Bob and N x 2N if Eve
+            if self.AB:
+                self.dense_depth = self.input_length - 1
+            else:
+                self.dense_depth = (2 * self.input_length) - 1
 
-        if self.concatenate_key:
-            self.con_input_layer = tf.concat([self.con_key, self.input_layer], axis=1)
-        else:
-            self.con_input_layer = self.input_layer
+            if self.concatenate_key:
+                self.con_input_layer = tf.concat(
+                    [self.con_key, self.input_layer], axis=1)
+            else:
+                self.con_input_layer = self.input_layer
 
-        # Dense layers for mixing the plaintext
-        self.dense_layers = tf.layers.dense(
-            inputs=self.con_input_layer,
-            units=self.input_length,
-            trainable=self.trainable
-        )
-
-        for i in range(0, self.dense_depth):
+            # Dense layers for mixing the plaintext
             self.dense_layers = tf.layers.dense(
-                inputs=self.dense_layers,
+                inputs=self.con_input_layer,
                 units=self.input_length,
                 trainable=self.trainable
             )
 
-        # Four conv layers for transforming the ciphertext
-        self.conv1 = tf.layers.conv1d(
-            inputs=self.dense_layers,
-            filters=2,
-            strides=self.strides[0],
-            kernel_size=[4],
-            padding='same',
-            activation=tf.sigmoid,
-            trainable=self.trainable
-        )
+            for i in range(0, self.dense_depth):
+                self.dense_layers = tf.layers.dense(
+                    inputs=self.dense_layers,
+                    units=self.input_length,
+                    trainable=self.trainable
+                )
 
-        self.conv2 = tf.layers.conv1d(
-            inputs=self.conv1,
-            filters=4,
-            strides=self.strides[1],
-            kernel_size=[2],
-            padding='same',
-            activation=tf.sigmoid,
-            trainable=self.trainable
-        )
+            # Four conv layers for transforming the ciphertext
+            self.conv1 = tf.layers.conv1d(
+                inputs=self.dense_layers,
+                filters=2,
+                strides=self.strides[0],
+                kernel_size=[4],
+                padding='same',
+                activation=tf.sigmoid,
+                trainable=self.trainable
+            )
 
-        self.conv3 = tf.layers.conv1d(
-            inputs=self.conv2,
-            filters=4,
-            strides=self.strides[2],
-            kernel_size=[1],
-            padding='same',
-            activation=tf.sigmoid,
-            trainable=self.trainable
-        )
+            self.conv2 = tf.layers.conv1d(
+                inputs=self.conv1,
+                filters=4,
+                strides=self.strides[1],
+                kernel_size=[2],
+                padding='same',
+                activation=tf.sigmoid,
+                trainable=self.trainable
+            )
 
-        self.conv4 = tf.layers.conv1d(
-            inputs=self.conv3,
-            filters=1,
-            strides=self.strides[3],
-            kernel_size=[1],
-            padding='same',
-            activation=tf.tanh,
-            trainable=self.trainable
-        )
+            self.conv3 = tf.layers.conv1d(
+                inputs=self.conv2,
+                filters=4,
+                strides=self.strides[2],
+                kernel_size=[1],
+                padding='same',
+                activation=tf.sigmoid,
+                trainable=self.trainable
+            )
 
-        self.output = tf.sign(tf.sign(self.conv4) + tf.ones_like(self.conv4) / 10)
-        return self.output
+            self.conv4 = tf.layers.conv1d(
+                inputs=self.conv3,
+                filters=1,
+                strides=self.strides[3],
+                kernel_size=[1],
+                padding='same',
+                activation=tf.tanh,
+                trainable=self.trainable
+            )
+
+            self.output = tf.sign(
+                tf.sign(self.conv4) + tf.ones_like(self.conv4) / 10)
+
+            return self.output
 
 
 def main():
@@ -117,12 +124,22 @@ def main():
     orig = tf.placeholder(dtype=tf.float32, shape=(None, num_bits, 1))
 
     print('Creating Alice net...')
-    train_alice = tf.placeholder(tf.bool)
-    alice_net = Model(bit_count=num_bits, concatenate_key=True, AB=True, training=train_alice)
+    alice_net = Model(
+        bit_count=num_bits, concatenate_key=True,
+        AB=True, training=True
+    )
+
     print('Creating Bob net...')
-    bob_net = Model(bit_count=num_bits, input_net=alice_net.network, concatenate_key=True, AB=True, training=True)
+    bob_net = Model(
+        bit_count=num_bits, input_net=alice_net.network,
+        concatenate_key=True, AB=True, training=True
+    )
+
     print('Creating Eve net...')
-    eve_net = Model(bit_count=num_bits, input_net=alice_net.network, AB=False, training=True)
+    eve_net = Model(
+        bit_count=num_bits, input_net=alice_net.network,
+        AB=False, training=True
+    )
 
     # Eves reconstruction error
     eve_loss = tf.reduce_sum(tf.abs(tf.subtract(orig, eve_net.network)))
@@ -137,11 +154,15 @@ def main():
     )
 
     bob_fancy_loss = bob_reconst - bob_eve  # not explicitly mentioned in the paper
-
     total_loss = bob_reconst - eve_loss
 
-    train_AB = tf.train.AdamOptimizer(learning_rate=0.0008).minimize(total_loss)
-    train_eve = tf.train.AdamOptimizer(learning_rate=0.0008).minimize(eve_loss)
+    AB_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Alice_Bob")
+    train_AB = tf.train.AdamOptimizer(learning_rate=0.0008).minimize(
+        total_loss, var_list=AB_varsB)
+
+    E_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Eve")
+    train_eve = tf.train.AdamOptimizer(learning_rate=0.0008).minimize(
+        eve_loss, var_list=E_vars)
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
@@ -149,8 +170,7 @@ def main():
 
         feed_dict = {
             alice_net.input_layer: train_X,
-            alice_net.con_key: keys,
-            train_alice: True
+            alice_net.con_key: keys
         }
 
         alice_out = sess.run(alice_net.network, feed_dict=feed_dict)
@@ -160,8 +180,7 @@ def main():
         feed_dict = {
             alice_net.input_layer: train_X,
             alice_net.con_key: keys,
-            bob_net.con_key: keys,
-            train_alice: True
+            bob_net.con_key: keys
         }
 
         bob_out = sess.run(bob_net.network, feed_dict=feed_dict)
@@ -173,8 +192,7 @@ def main():
                 alice_net.input_layer: train_X,
                 alice_net.con_key: keys,
                 bob_net.con_key: keys,
-                orig: train_X,
-                train_alice: True
+                orig: train_X
             }
 
             sess.run(train_AB, feed_dict=feed_dict_AB)
@@ -182,8 +200,7 @@ def main():
             feed_dict_E = {
                 alice_net.input_layer: train_X,
                 alice_net.con_key: keys,
-                orig: train_X,
-                train_alice: False
+                orig: train_X
             }
 
             sess.run(train_eve, feed_dict=feed_dict_E)
@@ -194,8 +211,8 @@ def main():
             print("    Eve reconstruction error: {} | Bob reconstruction error: {}".format(eve_error, bob_error))
 
             # get more messages
-            messages = get_plain_text(N=num_bits,to_generate=batch)
-            train_X = np.expand_dims(messages,axis = 2)
+            messages = get_plain_text(N=num_bits, to_generate=batch)
+            train_X = np.expand_dims(messages, axis=2)
 
 
 if __name__ == "__main__":
